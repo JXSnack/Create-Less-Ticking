f0467ec7e30ba35f69e307018648773438fd9377
JXSnack <jx.snack@gmail.com>
2024-08-16 15:22:07 +0200

    register on forge

--- forge/src/main/java/net/dadamalda/create/less_ticking/forge/CLTModForge.java
+++ forge/src/main/java/net/dadamalda/create/less_ticking/forge/CLTModForge.java
@@ -2,6 +2,7 @@ package net.dadamalda.create.less_ticking.forge;
 
 import net.dadamalda.create.less_ticking.CLTConfig;
 import net.dadamalda.create.less_ticking.CLTMod;
+import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -18,7 +19,12 @@ public class CLTModForge {
     }
 
     @SubscribeEvent
-    public void onServerStarting(FMLCommonSetupEvent event) {
+    public void onCommandSetup(FMLCommonSetupEvent event) {
         CLTMod.config = new CLTConfig(FMLPaths.CONFIGDIR.get().toAbsolutePath() + "/create_less_ticks.json");
+    }
+
+    @SubscribeEvent
+    public void onCommandRegister(RegisterCommandsEvent event) {
+        CLTMod.registerCommands(event.getDispatcher(), event.getBuildContext(), event.getCommandSelection());
     }
 }
