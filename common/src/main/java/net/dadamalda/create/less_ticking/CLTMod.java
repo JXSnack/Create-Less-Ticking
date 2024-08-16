a78b2c331b9f859239e2f7270f0b832c4a42fa81
JXSnack <jx.snack@gmail.com>
2024-08-16 15:20:43 +0200

    register command

--- common/src/main/java/net/dadamalda/create/less_ticking/CLTMod.java
+++ common/src/main/java/net/dadamalda/create/less_ticking/CLTMod.java
@@ -1,6 +1,12 @@
 package net.dadamalda.create.less_ticking;
 
+import com.mojang.brigadier.CommandDispatcher;
 import com.simibubi.create.Create;
+import net.minecraft.ChatFormatting;
+import net.minecraft.commands.CommandBuildContext;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,5 +24,18 @@ public class CLTMod {
 
     public static ResourceLocation id(String path) {
         return new ResourceLocation(MOD_ID, path);
+    }
+
+    public static void registerCommands(CommandDispatcher<CommandSourceStack> dispatcher,
+                                        CommandBuildContext ctx,
+                                        Commands.CommandSelection selection) {
+        dispatcher.register(Commands.literal("createlt")
+                .then(Commands.literal("reload")
+                        .executes((source) -> {
+                            source.getSource().sendSystemMessage(Component.literal("Config reloaded!").withStyle(ChatFormatting.GREEN));
+                            return 1;
+                        })
+                )
+        );
     }
 }
