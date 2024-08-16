afcb6881d2fffb73e274889c0dc0068271599e0e
JXSnack <jx.snack@gmail.com>
2024-08-16 15:20:48 +0200

    register commands on fabric

--- fabric/src/main/java/net/dadamalda/create/less_ticking/fabric/CLTFabric.java
+++ fabric/src/main/java/net/dadamalda/create/less_ticking/fabric/CLTFabric.java
@@ -4,6 +4,7 @@ import io.github.fabricators_of_create.porting_lib.util.EnvExecutor;
 import net.dadamalda.create.less_ticking.CLTConfig;
 import net.dadamalda.create.less_ticking.CLTMod;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.loader.api.FabricLoader;
 
 public class CLTFabric implements ModInitializer {
@@ -16,5 +17,6 @@ public class CLTFabric implements ModInitializer {
                 ), CLTMod.NAME);
 
         CLTMod.config = new CLTConfig(FabricLoader.getInstance().getConfigDir().toAbsolutePath() + "/create_less_ticks.json");
+        CommandRegistrationCallback.EVENT.register(CLTMod::registerCommands);
     }
 }
